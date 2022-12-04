dd8427fc293d33ad9dcfdc8d5c1b7734981dba1b
lorenzoo22 <lagudmarr@gmail.com>
2022-12-04 12:12:00 +0100

    cambios(II)

--- Api_Casas_Apuestas-main/main/services/__init__.py
+++ Api_Casas_Apuestas-main/main/services/__init__.py
@@ -1,6 +1,8 @@
-from .partidos import PartidoService
-from .empresa import EmpresaService
-from .apuesta import ApuestaService
-from .equipo import EquipoService
-from .cuota import CuotaService
-from .cliente import ClienteService
+import sys
+sys.path.insert(0,"C:/Users/Lorenzo/Documents/programacion/2.Desarrollo_OO/Apuestas_UEFA/Api_Casas_Apuestas-main/main/services")
+from partidos import PartidoService
+from empresa import EmpresaService
+from apuesta import ApuestaService
+from equipo import EquipoService
+from cuota import CuotaService
+from cliente import ClienteService
--- Api_Casas_Apuestas-main/main/services/decorators.py
+++ Api_Casas_Apuestas-main/main/services/decorators.py
@@ -32,7 +32,7 @@ def validar_equipo_pro(*ids):
                 equipo = db.session.query(EquipoModel).get(id)
                 if not equipo:
                     return f'El equipo con el id: {id} no ha sido encontrado', 404
-                    break
+    
             return function(*args, **kwargs)
         return wrapper
     return decorator
