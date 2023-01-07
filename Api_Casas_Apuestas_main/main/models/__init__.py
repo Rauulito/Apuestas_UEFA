7dabfd0d3ac7b04d8029b5f658b6cba7ad7cb10d
lorenzoo22 <lagudmarr@gmail.com>
2023-01-07 05:57:19 +0100

    Cambios en las importaciones + explicacion

--- Api_Casas_Apuestas_main/main/models/__init__.py
+++ Api_Casas_Apuestas_main/main/models/__init__.py
@@ -4,3 +4,11 @@ from .empresa import Empresa as EmpresaModel
 from .partido import Partido as PartidoModel
 from .equipo import Equipo as EquipoModel
 from .cuota import Cuota as CuotaModel
+
+
+"""
+IMPORTANTE--> ¿Cómo ejecutar todos estos archivos con las importaciones relativas incorporadas?
+Desde un archivo bajo el cual sean conocidos los arhivos se ejecuta el siguiente comando:
+python -m main.models."archivo a ejecutar"
+De esta manera se evita el error:   "ImportError: attempted relative import with no known parent package"
+"""
